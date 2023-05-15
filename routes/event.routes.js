a4e2aa48dc58ee7e16be0c436bea09acd024e8e5
Manuel Pérez <mperezpradosjose@gmail.com>
2023-05-15 16:09:28 +0200

    hola

--- routes/event.routes.js
+++ routes/event.routes.js
@@ -2,13 +2,15 @@ const express = require('express');
 const router = require("express").Router()
 const Event = require("../models/Event.model")
 
+const { isLoggedIn, checkRoles } = require('../middlewares/route-guard')
 
-router.get('/event/list', (req, res, next) => {
+// event list
+router.get('/event/list', isLoggedIn, (req, res, next) => {
 
-    // const userRole = {
-    //     isUSER: req.session.currentUser?.role === 'USER',
-    //     isADMIN: req.session.currentUser?.role === 'ADMIN'
-    // }
+    const userRole = {
+        isUser: req.session.currentUser?.role === 'USER',
+        isAdmin: req.session.currentUser?.role === 'ADMIN'
+    }
 
     Event
         .find()
@@ -19,12 +21,12 @@ router.get('/event/list', (req, res, next) => {
 
 // Create Event//
 // render
-router.get('/event/event-create', (req, res, next) => {
+router.get('/event/event-create', isLoggedIn, checkRoles('ADMIN'), (req, res, next) => {
     res.render('event/event-create')
 
 })
 //handler
-router.post('/event/event-create', (req, res, next) => {
+router.post('/event/event-create', isLoggedIn, checkRoles('ADMIN'), (req, res, next) => {
     const { title, description, category, start, end, city, lat, lng, eventImg, } = req.body;
     const location = {
         type: 'Point',
@@ -37,9 +39,6 @@ router.post('/event/event-create', (req, res, next) => {
 
 })
 
-//Perfil
-router.get('/user/profile', (req, res, next) => {
-    res.render('user/profile')
-})
+
 
 module.exports = router;
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -2,5 +2,10 @@ const express = require('express');
 const router = require("express").Router()
 
 
+//Perfil
+router.get('/user/profile', (req, res, next) => {
+    res.render('user/profile')
+})
+
 
 module.exports = router;
