dab59bfca10c624d17946e02e4a06a9314e91530
Manuel Pérez <mperezpradosjose@gmail.com>
2023-05-15 17:57:04 +0200

    efer

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const router = require("express").Router()
 
+const User = require("../models/User.model");
+const { isLoggedIn } = require('../middlewares/route-guard');
+
 
 //Perfil
-router.get('/user/profile', (req, res, next) => {
-    res.render('user/profile')
+
+
+router.get('/user/:_id', isLoggedIn, (req, res) => {
+    const id = req.session.currentUser._id
+    User.findById(id)
+        .then(user => res.render('user/profile', user))
+        .catch(err => console.log(err))
 })
+
 
 
 module.exports = router;
