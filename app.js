4836c6f0b5e9a700e311b1eb0491a2d1f9855df8
Manuel Pérez <mperezpradosjose@gmail.com>
2023-05-18 10:32:59 +0200

    ok

--- app.js
+++ app.js
@@ -8,41 +8,12 @@ const session = require("express-session");
 
 const { loggedUsers } = require('./middlewares/user-guard')
 
-<<<<<<< HEAD
 require("./config")(app);
 require('./config/session.config')(app)
 
 require("./routes/index")(app)
 //     req.locals.isAdmin = req.session.currentUser?.role === 'ADMIN'
 // app.use(loggedUsers);
-=======
-
-require("./config")(app);
-require('./config/session.config')(app)
-
-
-// app.use((req, res, next) => {
-//     // const loggedUser = req.session.currentUser;
-//     // const userAdmin = req.session.currentUser?.role
-//     // const userAvatar = req.session.currentUser?.avatar
-
-//     // if (loggedUser) {
-//     //     res.locals.hideLogin = true;
-//     // auth    res.locals.hideSignUp = true;
-//     // }
-
-//     // serAdmin === 'ADMIN') res.locals.isAdmin = true;
-
-//     // if (userAvatar) {
-//     //     res.locals.isAvatar = true;
-//     //     res.locals.avatarUrl = userAvatar;
-//     // }
-
-//     req.locals.loggedUser = req.session.currentUser
-//     req.locals.isAdmin = req.session.currentUser?.role === 'ADMIN'
-
-//     next();
->>>>>>> Manuelp
 
 
 
